77a509039b941bc66eb7ed6672e5eb32fa81c50e
Mostakimul Karim <mostakimulkarim@gmail.com>
2024-04-20 14:50:15 -0400

    [feat]: redux setup

--- naveen-client/src/main.jsx
+++ naveen-client/src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
+import { store } from './redux/store';
 import routes from './routes/routes';
 import ThemeProvider from './utils/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={routes} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <RouterProvider router={routes} />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 );
